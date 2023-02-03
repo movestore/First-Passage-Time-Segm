8fa0dc4983a943625708ac32d09875390e0d4884
annescharf <ascharf@ab.mpg.de>
2023-02-03 14:16:29 +0100

    plot updates automatically

--- ShinyModule.R
+++ ShinyModule.R
@@ -10,7 +10,7 @@ library("shinyBS")
 Sys.setenv(tz="UTC") 
 
 ## improve layout of input buttons
-## when indiv is selected, that it stays selected and automatically updates the plot. Started but did not find solution yet. For now all is commented
+## when indiv is selected, that it stays selected and automatically updates the plot.  ==> plot updates, indiv does not stay marked...
 
 
 shinyModuleUserInterface <- function(id, label) {
@@ -51,13 +51,8 @@ shinyModule <- function(input, output, session, data) {
                 value = input$time_thr, min = 0, max = input$time_thr*5)
   })
   
-  ## not sure if this is the most elegant way of doing it, but may work...
   currentANN <- reactiveVal() ## before hitting the go button, this object is empty, the input dataset will be saved as output
-  # overview_named_r <- reactiveVal()
-  # fpt_sel_r <- reactiveVal()
-  # data_sel_r <- reactiveVal()
-  # mfpt_r <- reactiveVal()
-  # 
+  
   observeEvent(input$goButton, {
     data.split <- move::split(data)
     mfpt <- foreach(datai = data.split) %do%
@@ -103,6 +98,7 @@ shinyModule <- function(input, output, session, data) {
       as.character(max(timestamps(datai)))
     }
     
+    
     overview <- data.frame(ids,tags,time0,timeE)
     N <- length(overview[1,])
     for (i in seq_len(nrow(overview))){
@@ -114,84 +110,69 @@ shinyModule <- function(input, output, session, data) {
     }
     overview <- overview[,c(N+1,1:N)]
     
-    overview_named <- overview
-    names(overview_named) <- c("Show Plots and Map","Animal","Tag","First timestamp","Last timestamp")
-    # overview_named_r(overview_named)
-    # mfpt_r(mfpt)
-  # })
-  
-  # observeEvent(input$C, {
-    selID <- if(is.null(input$C)){overview$ids[1]}else{input$C}
     
-    fpt_sel <- mfpt[[selID]]
     
-    data_sel <- data.split[[selID]]
-    
-    behav <- rep(NA,length(data_sel))
-    behav[fpt_sel$fpt_val<input$time_thr & !is.na(fpt_sel$fpt_val)] <- 1 #fast movement
-    behav[fpt_sel$fpt_val>=input$time_thr & !is.na(fpt_sel$fpt_val)] <- 2 #slow movement/resting
-    time <- timestamps(data_sel)
-    beh <- data.frame(behav,time)
-    
-    data_sel_mig <- data_sel[which(beh$behav==1)]
-    
-    data_sel_rest <- data_sel[which(beh$behav==2)]
-    
-    # fpt_sel_r(fpt_sel)
-    # data_sel_r(data_sel)
-  
-  # })
-  
-    output$foo <- DT::renderDataTable(
-      # if(input$goButton==0){data.frame(ShowPlotsAndMap=numeric(), Animal=character(), Tag=numeric,FirstTimestamp=numeric, LastTimestamp=numeric, stringsAsFactors=FALSE)}else{overview_named_r()},
-      overview_named,
-      escape = FALSE,
-      selection = 'none',
-      server = FALSE,
-      options = list(dom = 't', paging = FALSE, ordering = FALSE, rownames=FALSE, scrollY=250, scrollCollapse = TRUE)
-    )
-    
-    output$fpt <- renderPlot({
-      # if(input$goButton==0){plot(1,1,type="n",xaxt='n',yaxt='n', ann=FALSE)
-        # }else{
-          # fpt_sel_p <- fpt_sel_r()
-          fpt_sel_p <- fpt_sel
-      plot(fpt_sel_p$time,fpt_sel_p$fpt_val,type="l",main=selID,ylab=paste0("FPT (",input$thr_uni,")"),xlab="time")
-      points(fpt_sel_p$time[beh==1],fpt_sel_p$fpt_val[beh==1],cex=2,pch=20,col="red")
-      points(fpt_sel_p$time[beh==2],fpt_sel_p$fpt_val[beh==2],cex=2,pch=20,col="darkgreen")
-      abline(h=input$time_thr,col="blue",lwd=2)
-      # }
-    })
-    
-    output$leafmap <- renderLeaflet({
-      # if(input$goButton==0){leaflet()
-      # }else{
-        # data_sel_p <- data_sel_r()
-        data_sel_p <- data_sel
-      bounds <- as.vector(bbox(extent(data_sel_p)))
-      outl <- leaflet() %>%
-        fitBounds(bounds[1], bounds[2], bounds[3], bounds[4]) %>%
-        addTiles() %>%
-        addProviderTiles("Esri.WorldTopoMap",group = "TopoMap") %>%
-        addProviderTiles("Esri.WorldImagery", group = "Aerial") %>%
-        addPolylines(data =  coordinates(data_sel_p), color ="blue", group = "lines") %>%
-        addCircles(data = data_sel_mig, fillOpacity = 0.3, opacity = 0.5, color="red", group = "fast movement") %>%
-        addCircles(data = data_sel_rest, fillOpacity = 0.3, opacity = 0.5, color="darkgreen", group = "slow movement") %>%
-        addLegend(position= "topright", colors=c("blue","red","darkgreen"),
-                  labels=c("lines","fast movement","slow movement") ,opacity = 0.7, title = paste("track",unique(namesIndiv(data_sel)))) %>%
-        addScaleBar(position="bottomleft",
-                    options=scaleBarOptions(maxWidth = 100,
-                                            metric = TRUE, imperial = F, updateWhenIdle = TRUE)) %>%
-        addLayersControl(
-          baseGroups = c("StreetMap", "Aerial"),
-          overlayGroups = c("lines", "fast movement","slow movement"),
-          options = layersControlOptions(collapsed = FALSE)
-        )
-      outl
-      # }
+    observe({
+      overview_named <- overview
+      names(overview_named) <- c("Show Plots and Map","Animal","Tag","First timestamp","Last timestamp")
+      
+      selID <- if(is.null(input$C)){overview$ids[1]}else{input$C}
+      
+      fpt_sel <- mfpt[[selID]]
+      
+      data_sel <- data.split[[selID]]
+      
+      behav <- rep(NA,length(data_sel))
+      behav[fpt_sel$fpt_val<input$time_thr & !is.na(fpt_sel$fpt_val)] <- 1 #fast movement
+      behav[fpt_sel$fpt_val>=input$time_thr & !is.na(fpt_sel$fpt_val)] <- 2 #slow movement/resting
+      time <- timestamps(data_sel)
+      beh <- data.frame(behav,time)
+      
+      data_sel_mig <- data_sel[which(beh$behav==1)]
+      
+      data_sel_rest <- data_sel[which(beh$behav==2)]
+      
+      
+      output$foo <- DT::renderDataTable(
+        overview_named,
+        escape = FALSE,
+        selection = 'none',
+        server = FALSE,
+        options = list(dom = 't', paging = FALSE, ordering = FALSE, rownames=FALSE, scrollY=250, scrollCollapse = TRUE)
+      )
+      
+      output$fpt <- renderPlot({
+        plot(fpt_sel$time,fpt_sel$fpt_val,type="l",main=selID,ylab=paste0("FPT (",input$thr_uni,")"),xlab="time")
+        points(fpt_sel$time[beh==1],fpt_sel$fpt_val[beh==1],cex=2,pch=20,col="red")
+        points(fpt_sel$time[beh==2],fpt_sel$fpt_val[beh==2],cex=2,pch=20,col="darkgreen")
+        abline(h=input$time_thr,col="blue",lwd=2)
+      })
+      
+      output$leafmap <- renderLeaflet({
+        bounds <- as.vector(bbox(extent(data_sel)))
+        outl <- leaflet() %>%
+          fitBounds(bounds[1], bounds[2], bounds[3], bounds[4]) %>%
+          addTiles() %>%
+          addProviderTiles("Esri.WorldTopoMap",group = "TopoMap") %>%
+          addProviderTiles("Esri.WorldImagery", group = "Aerial") %>%
+          addPolylines(data =  coordinates(data_sel), color ="blue", group = "lines") %>%
+          addCircles(data = data_sel_mig, fillOpacity = 0.3, opacity = 0.5, color="red", group = "fast movement") %>%
+          addCircles(data = data_sel_rest, fillOpacity = 0.3, opacity = 0.5, color="darkgreen", group = "slow movement") %>%
+          addLegend(position= "topright", colors=c("blue","red","darkgreen"),
+                    labels=c("lines","fast movement","slow movement") ,opacity = 0.7, title = paste("track",unique(namesIndiv(data_sel)))) %>%
+          addScaleBar(position="bottomleft",
+                      options=scaleBarOptions(maxWidth = 100,
+                                              metric = TRUE, imperial = F, updateWhenIdle = TRUE)) %>%
+          addLayersControl(
+            baseGroups = c("StreetMap", "Aerial"),
+            overlayGroups = c("lines", "fast movement","slow movement"),
+            options = layersControlOptions(collapsed = FALSE)
+          )
+        outl
+      })
     })
   })
- 
+  
   reactive({ if((length(currentANN())>=1)){return(currentANN() )}else{return(current())} })
   
 }
