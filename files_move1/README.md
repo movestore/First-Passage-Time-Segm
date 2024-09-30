483bc4e7791d5df5bd0fae833504e1a9d78c9e00
andreakoelzsch <akoelzsch@ab.mpg.de>
2024-09-30 14:56:07 +0200

    add move1 files

---
+++ files_move1/README.md
@@ -0,0 +1,45 @@
+# First Passage Time Segmentation
+
+MoveApps
+
+Github repository: *github.com/movestore/First-Passage-Time-Segm*
+
+## Description
+This App calculated the first passage time of all individual tracks. Depending on a time threshold, a map overview indicates for each animal which locations lie below (fast movement) and above (slow movement/resting) such a time.
+
+## Documentation
+Given one radius distance, this App calculated for each location of each animal the time it needs to cross this radius, i.e. the first passage time (FPT). This App uses the fpt() function from the adehabitatLT package.
+
+For each animals it is possible to plot the FPT for each location with a time threshold line. This time threshold has to be initially defined and can be adapted in the User Interface (Shiny UI).
+
+Depending on the time threshold, this App annotates each location with NA (not possible to calculate FPT), fast movement (low FPT) and slow movement/resting (high FPT). On a map the track of a selected animals is plotted together with the locations in their respective colour (NA is omitted).
+
+For each location, the fpt value and behaviour (fast/slow movement) are annotated and returned in the output rds data set in the columns named "fpt_value" and "fpt_behaviour" respectively. That way they can be used in subsequent Apps, including csv file creating (rds2csv App) or shapfile creation (Write Shapefile).
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+moveStack in Movebank format
+Shiny user interface (UI)
+
+### Artefacts
+none
+
+### Settings 
+`Radius parameter for First Passage Time`: Define the radius for which you want to calculate the first passage times (when does an animal pass the radius). Unit: `metres`. Default 30000 m = 30 km.
+
+`Threshold time for migration/resting`: Define the time threshold with which you will split your tracks into fast and slow movement locations. This is the minimum time your animals need to pass a given radius (above) if resting. Select your time unit below. Default 10 (days).
+
+`Unit of the threshold time`: Select the time unit of your selected threshold time. Possible values `seconds`, `hours`, and `days`. Default `days`.
+
+`Start/Update calculation`: after customizing or changing any of the parameters above, hit this button to do the calculation using the new input.
+
+### Null or error handling:
+**Setting `Radius parameter for First Passage Time`:** A default of 30 km is provided, but can be changed to NULL, which will lead to an error. Take care to relate this parameter to the extent of your data set. If the radius is too large for the data set then all FPT values will be NA. If the radius is too small then FPTs will be extremely short. Negative values will not be tolerated and lead to an error.
+
+**Setting `Threshold time for migration/resting`:** The time threshold has to be positive and not NULL, a default of 10 days is provided. By shifting the slider in the UI, this parameter can be changed, but only to integer values. Initial values can be double. Negative values are not tolerated and lead to an error.
+
+**Setting `Unit of the threshold time`:** Radiobuttons with three possible values and default (see above). No null or error possibilities.
+
+**Data:** The data are not manipulated in this App, but interactively explored. So that a possible Workflow can be continued after this App, the input data set is returned with the columns "fpt_value" and "fpt_behaviour" appended.
---
+++ files_move1/ShinyModule.R
@@ -0,0 +1,179 @@
+library('move')
+library('ggmap')
+library('adehabitatLT')
+library('shiny')
+library('leaflet')
+library('DT')
+library('foreach')
+library("shinyBS")
+
+Sys.setenv(tz="UTC") 
+
+## improve layout of input buttons
+## when indiv is selected, that it stays selected and automatically updates the plot.  ==> plot updates, indiv does not stay marked...
+
+
+shinyModuleUserInterface <- function(id, label) {
+  ns <- NS(id)
+  
+  tagList(
+    titlePanel("First Passage Time Segmentation"),
+    fluidRow(
+      column(3,numericInput(ns("radius"), label="Radius parameter for First Passage Time", value=30000),
+             bsTooltip(id=ns("radius"), title="Define the radius for which you want to calculate the first passage times (when does an animal pass the radius). Unit = m.", placement = "bottom", trigger = "hover", options = list(container = "body")),
+             
+             numericInput(ns("time_thr"),label="Threshold time for migration/resting", value=10),
+             bsTooltip(id=ns("time_thr"), title="Define the time threshold with which you will split your tracks into fast and slow movement locations. This is the minimum time your animals need to pass a given radius (above) if resting. Select your time unit below.", placement = "bottom", trigger = "hover", options = list(container = "body")),
+             
+             selectInput(ns("thr_uni"), label="Unit of the threshold time", choices=c("seconds", "hours","days"),selected="days"),
+             bsTooltip(id=ns("thr_uni"), title="Select the time unit of your selected threshold time.", placement = "bottom", trigger = "hover", options = list(container = "body"))),
+      
+      column(3,uiOutput(ns("sliderOption"))),
+      
+      column(3,actionButton(ns("goButton"),"Start/Update calculation")),
+    ),
+    div(id=ns("C"),class='shiny-input-radiogroup',DT::dataTableOutput(ns("foo"))),
+    
+    fluidRow(
+      column(width=5, plotOutput(ns("fpt"),height="500px")),
+      column(width=7, leafletOutput(ns("leafmap"),height="500px"))
+    )
+  )
+}
+
+
+shinyModule <- function(input, output, session, data) {
+  ns <- session$ns
+  current <- reactiveVal(data)
+  output$sliderOption <-renderUI({
+    sliderInput(inputId = ns("time_thr"), 
+                label = paste("Time threshold for passing out of",input$radius,"m radius (unit =",input$thr_uni,") to split fast movement from slow movement/resting"), 
+                value = input$time_thr, min = 0, max = input$time_thr*5)
+  })
+  
+  currentANN <- reactiveVal() ## before hitting the go button, this object is empty, the input dataset will be saved as output
+  
+  observeEvent(input$goButton, {
+    data.split <- move::split(data)
+    mfpt <- foreach(datai = data.split) %do%
+      {
+        datait <- spTransform(datai,center=T)
+        track <- as.ltraj("xy"=data.frame("x"=coordinates(datait)[,1],"y"=coordinates(datait)[,2]),"date"=as.POSIXct(timestamps(datait)),"id"=namesIndiv(datait),"typeII"=TRUE,"proj4string"=CRS(projection(datait)))
+        
+        f <- fpt(track,radii=input$radius,units=input$thr_uni)
+        fpt_val <- f[[1]][,1]
+        time <- as.POSIXct(timestamps(datait))
+        data.frame(fpt_val,time)
+      }
+    
+    names(mfpt) <- namesIndiv(data) 
+    
+    
+    dataiANN <- foreach(datai = data.split) %do%
+      {
+        naam <- namesIndiv(datai)
+        datai@data <- cbind(datai@data,"fpt_value"=mfpt[[which(names(mfpt)==naam)]]$fpt_val)
+        
+        datai@data$fpt_behaviour <- NA
+        datai@data$fpt_behaviour[datai@data$fpt_value>input$time_thr] <- "slow"
+        datai@data$fpt_behaviour[datai@data$fpt_value<=input$time_thr] <- "fast"
+        datai
+      }
+    
+    currentANN(moveStack(dataiANN,forceTz="UTC"))
+    
+    
+    ids <- namesIndiv(data)
+    tags <- foreach(datai = data.split, .combine=c) %do% {
+      if(is.null(datai@idData$tag.local.identifier[1])){
+        datai$tag.local.identifier[1]
+      }else{
+        datai@idData$tag.local.identifier[1] ### hope this takes care of the possible errors
+      }
+    }
+    time0 <- foreach(datai = data.split, .combine=c) %do% {
+      as.character(min(timestamps(datai)))
+    }
+    timeE <- foreach(datai = data.split, .combine=c) %do% {
+      as.character(max(timestamps(datai)))
+    }
+    
+    
+    overview <- data.frame(ids,tags,time0,timeE)
+    N <- length(overview[1,])
+    for (i in seq_len(nrow(overview))){
+      overview[i,N+1] <- sprintf(
+        '<input type="radio" name="%s" value="%s"/>',
+        session$ns("C"),ids[i]
+      )
+      names(overview)[N+1] <- "show"
+    }
+    overview <- overview[,c(N+1,1:N)]
+    
+    
+    
+    observe({
+      overview_named <- overview
+      names(overview_named) <- c("Show Plots and Map","Animal","Tag","First timestamp","Last timestamp")
+      
+      selID <- if(is.null(input$C)){overview$ids[1]}else{input$C}
+      
+      fpt_sel <- mfpt[[selID]]
+      
+      data_sel <- data.split[[selID]]
+      
+      behav <- rep(NA,length(data_sel))
+      behav[fpt_sel$fpt_val<input$time_thr & !is.na(fpt_sel$fpt_val)] <- 1 #fast movement
+      behav[fpt_sel$fpt_val>=input$time_thr & !is.na(fpt_sel$fpt_val)] <- 2 #slow movement/resting
+      time <- timestamps(data_sel)
+      beh <- data.frame(behav,time)
+      
+      data_sel_mig <- data_sel[which(beh$behav==1)]
+      
+      data_sel_rest <- data_sel[which(beh$behav==2)]
+      
+      
+      output$foo <- DT::renderDataTable(
+        overview_named,
+        escape = FALSE,
+        selection = 'none',
+        server = FALSE,
+        options = list(dom = 't', paging = FALSE, ordering = FALSE, rownames=FALSE, scrollY=250, scrollCollapse = TRUE)
+      )
+      
+      output$fpt <- renderPlot({
+        plot(fpt_sel$time,fpt_sel$fpt_val,type="l",main=selID,ylab=paste0("FPT (",input$thr_uni,")"),xlab="time")
+        points(fpt_sel$time[beh==1],fpt_sel$fpt_val[beh==1],cex=2,pch=20,col="red")
+        points(fpt_sel$time[beh==2],fpt_sel$fpt_val[beh==2],cex=2,pch=20,col="darkgreen")
+        abline(h=input$time_thr,col="blue",lwd=2)
+      })
+      
+      output$leafmap <- renderLeaflet({
+        bounds <- as.vector(bbox(extent(data_sel)))
+        outl <- leaflet() %>%
+          fitBounds(bounds[1], bounds[2], bounds[3], bounds[4]) %>%
+          addTiles() %>%
+          addProviderTiles("Esri.WorldTopoMap",group = "TopoMap") %>%
+          addProviderTiles("Esri.WorldImagery", group = "Aerial") %>%
+          addPolylines(data =  coordinates(data_sel), color ="blue", group = "lines") %>%
+          addCircles(data = data_sel_mig, fillOpacity = 0.3, opacity = 0.5, color="red", group = "fast movement") %>%
+          addCircles(data = data_sel_rest, fillOpacity = 0.3, opacity = 0.5, color="darkgreen", group = "slow movement") %>%
+          addLegend(position= "topright", colors=c("blue","red","darkgreen"),
+                    labels=c("lines","fast movement","slow movement") ,opacity = 0.7, title = paste("track",unique(namesIndiv(data_sel)))) %>%
+          addScaleBar(position="bottomleft",
+                      options=scaleBarOptions(maxWidth = 100,
+                                              metric = TRUE, imperial = F, updateWhenIdle = TRUE)) %>%
+          addLayersControl(
+            baseGroups = c("StreetMap", "Aerial"),
+            overlayGroups = c("lines", "fast movement","slow movement"),
+            options = layersControlOptions(collapsed = FALSE)
+          )
+        outl
+      })
+    })
+  })
+  
+  reactive({ if((length(currentANN())>=1)){return(currentANN() )}else{return(current())} })
+  
+}
+
